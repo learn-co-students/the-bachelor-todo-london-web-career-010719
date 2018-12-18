8aa8608177f9eb94d063c7cffe5ef5331a234354
Joseph O'Dell <josephodell@outlook.com>
2018-12-18 17:19:41 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,38 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+data.each do |seasons, bio|
+  if seasons == season
+    bio.each {|hash| return hash["name"].split(" ")[0] if hash["status"] == "Winner"}
+  end
+end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+data.each do |seasons, bio|
+  bio.each {|hash| return hash["name"] if hash["occupation"] == occupation}
+end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+counter = 0
+data.each do |seasons, bio|
+  bio.each {|hash| counter += 1 if hash["hometown"] == hometown}
+end
+counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |seasons, bio|
+    bio.each {|hash| return hash["occupation"] if hash["hometown"] == hometown}
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+array = []
+data.each do |seasons, bio|
+    bio.each {|hash| array << hash["age"]} if seasons == season
+end
+sum= 0.0
+array.each {|x| sum += x.to_i}
+sum = sum/ array.size
+sum.round
 end
