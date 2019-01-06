74258be7ffb4c627fda4a8e3d930886556814530
Songyishu Yang <songyishu.yang@gmail.com>
2019-01-06 14:37:45 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,50 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestant|
+    if contestant.has_value?("Winner")
+      return contestant.values[0].split(" ")[0]
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  name = ""
+  data.each do |season_name,season_people|
+    season_people.map do |contestant|
+      if contestant.has_value?(occupation)
+        return contestant.values[0]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season_name,season_people|
+    season_people.each do |contestant|
+      if contestant.has_value?(hometown)
+        counter += 1
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season_name,season_people|
+    season_people.map do |contestant|
+      if contestant.has_value?(hometown)
+        return contestant.values[3]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  total_age = 0
+  number_of_people = 0
+  data[season].each do |contestant|
+    total_age += contestant.values[1].to_f
+    number_of_people += 1
+  end
+  return (total_age/number_of_people).round
 end
